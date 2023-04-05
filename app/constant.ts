698fd8fa4b715bc26900d822a7261bdcedf300f4
ZHtwinkle <79469234+ZHtwinkle@users.noreply.github.com>
2023-04-05 14:52:52 +0800

    Add files via upload

---
+++ app/constant.ts
@@ -0,0 +1,5 @@
+export const OWNER = "Yidadaa";
+export const REPO = "ChatGPT-Next-Web";
+export const REPO_URL = `http://blog.zhtwinkle.cn/`;
+export const UPDATE_URL = `http://blog.zhtwinkle.cn/`;
+export const FETCH_COMMIT_URL = `https://api.github.com/repos/${OWNER}/${REPO}/commits?per_page=1`;
